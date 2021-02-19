d88b7610c7ccd03101e82f00ff03812150e7c8bf
AerialWombat <adrianzhan@gmail.com>
2021-02-19 13:37:53 -0500

    add region icons to filter buttons

--- frontend/src/components/FilterButton.tsx
+++ frontend/src/components/FilterButton.tsx
@@ -6,12 +6,27 @@ interface Props {
 }
 
 const FilterButton: React.FC<Props> = ({ type, value, filters, clickHandler }) => {
+  const regionIconImageUrl = `${
+    process.env.PUBLIC_URL
+  }/images/icons/icon-${value.toLowerCase()}.png`;
+
+  console.log('ICON URL:', regionIconImageUrl);
+
   return (
     <div
       className={`${type}-filter` + (filters[value] ? ` ${type}-filter--active` : '')}
       onClick={(event) => clickHandler(value)}
     >
-      {value}
+      {type === 'cost' ? (
+        value
+      ) : (
+        <img
+          loading="lazy"
+          className="region-filter__icon"
+          src={regionIconImageUrl}
+          alt={`${value} icon`}
+        />
+      )}
     </div>
   );
 };
